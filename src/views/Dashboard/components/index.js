1949a4be9378d731a3cae6fa15782e5ca2ed9be0
Santiago.Gonzalez <santiago.gonzalezocourel@axpo.com>
2021-05-28 20:37:38 +0200

    first commit

---
+++ src/views/Dashboard/components/index.js
@@ -0,0 +1,10 @@
+export { default as Attendance } from './Attendance';
+export { default as LatestOrders } from './LatestOrders';
+export { default as LatestEntries } from './LatestEntries';
+export { default as LatestProducts } from './LatestProducts';
+export { default as LatestSales } from './LatestSales';
+export { default as TasksProgress } from './TasksProgress';
+export { default as TotalProfit } from './TotalProfit';
+export { default as TotalSubscribers } from './TotalSubscribers';
+export { default as TotalSubscriptions } from './TotalSubscriptions';
+
